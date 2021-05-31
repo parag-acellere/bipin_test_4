5ac8f53a3bbebacb4ae9b30886c8eea81dc7c4c4
Bipin Patel <bipin.patel@acellere.com>
2021-05-31 17:36:37 +0530

    Update helloworld.js

--- helloworld.js
+++ helloworld.js
@@ -3,3 +3,17 @@ console.log('Test');
 var a=10;
 var b=4;
 debugger;
+
+switch (foo) {
+    case 1:
+        doSomething();
+        break;
+
+    case 2:
+        doSomething();
+        break;
+
+    default:
+        // do nothing
+}
+var num = new {}();
